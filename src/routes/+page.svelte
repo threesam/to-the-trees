15e1a544073b03b06e780650c4ccafa8d8cb4461
Sam <57677512+threesam@users.noreply.github.com>
2023-04-25 13:00:35 -0400

    organizes sections, adds site info/mission statement section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,18 +14,20 @@
 	onMount(() => (show = true));
 </script>
 
+<!-- HERO -->
 <section id="hero" class="relative w-full flex items-center justify-center h-screen">
 	<img
 		class="w-full h-full object-cover absolute inset-0"
 		src={urlFor(data.settings.image.asset.url).auto('format').url()}
 		alt="homepage hero" />
-	<div class=" absolute inset-0 bg-gradient-fade" />
+	<div class=" absolute inset-0 bg-gradient-fade opacity-60" />
 	<h1 class="z-0 w-full flex justify-center lg:grid lg:grid-cols-2 gap-10 lg:text-6xl">
 		<span class="text-light lg:text-right">Laila</span>
 		<span class="text-light lg:text-left">Wolf</span>
 	</h1>
 </section>
 
+<!-- SUBSCRIBE + LINKS -->
 <section class="flex flex-col-reverse lg:grid lg:grid-cols-2">
 	<div class="aspect-square grid place-content-center bg-white p-5">
 		<SubscribeForm />
@@ -35,6 +37,12 @@
 	</div>
 </section>
 
+<!-- ABOUT SITE -->
+<div class="bg-gradient-3 bg-light lg:py-24">
+	<PortableText blocks={data.settings.body} />
+</div>
+
+<!-- ABOUT AUTHOR - HEADER -->
 <section class="grid grid-cols-2">
 	<h1 class="grid place-content-center text-2xl lg:text-5xl">About</h1>
 	<img
@@ -42,6 +50,7 @@
 		alt={data.settings.founders[0].image} />
 </section>
 
+<!-- ABOUT AUTHOR - TEXT -->
 <div class="bg-gradient-3 bg-light">
-	<PortableText blocks={data.settings.body} />
+	<PortableText blocks={data.settings.founders[0].bio} />
 </div>
