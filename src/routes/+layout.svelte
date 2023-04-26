7b72b1381d3c84d26456df838b4f71155cbdecf0
Sam <57677512+threesam@users.noreply.github.com>
2023-04-26 00:00:21 -0400

    adds hidden field

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -30,6 +30,7 @@
 		class="p-5 lg:p-10 bg-black z-0 border-2 w-96 max-w-full border-white flex flex-col gap-5
 		lg:gap-10 transition-all duration-300">
 		<h4 class="text-7xl tracking-widest">Contact</h4>
+		<input type="hidden" name="form-name" value="contact" />
 		<label for="email">
 			<input
 				class="p-5 w-full bg-black rounded-none text-white border-2 border-white focus:outline-none
