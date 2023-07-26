30c7ffa7fca5a569de0e49a2b8133c000d4b7132
Sam <57677512+threesam@users.noreply.github.com>
2023-07-26 13:51:54 -0400

    adds quote component

---
+++ src/lib/components/Quote.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { CustomBlockComponentProps } from '@portabletext/svelte'
+
+	export let portableText: CustomBlockComponentProps
+	console.log('portableText: ', portableText)
+</script>
+
+<blockquote class="mb-5 mt-8 bg-primary p-5 text-base text-dark">
+	{portableText.value.children[0].text}
+</blockquote>
