d50aa786ee5f8c3b4a5bd8b99b842a43cc8aac84
Sam <57677512+threesam@users.noreply.github.com>
2023-08-10 10:44:05 -0400

    conditionally render link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -69,7 +69,13 @@
 			href={film.gumroadLink ?? '#'}
 			class="relative z-0 flex h-full w-full items-center justify-center bg-transparent p-5 transition-colors duration-300 hover:bg-primary sm:p-10"
 		>
-			<span class="bg-primary p-3 text-dark sm:p-8 sm:font-bold"> WATCH NOW </span>
+			<span class="bg-primary p-3 text-dark sm:p-8 sm:font-bold">
+				{#if film.gumroadLink}
+					WATCH NOW
+				{:else}
+					COMING SOON
+				{/if}
+			</span>
 		</a>
 	</div>
 </section>
